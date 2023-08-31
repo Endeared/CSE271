f40518f4a77f322a4c5e806cb5447ddb0f9d12df
Ross <rosshamey@gmail.com>
2023-08-31 11:55:38 -0400

    whoopsie forgot to remove prints

--- DD_Util_Ross_Two.java
+++ DD_Util_Ross_Two.java
@@ -6,13 +6,11 @@ import java.io.*;
 public class DD_Util_Ross_Two {
 
    public static void main(String[] args){
-       int[] b = randomIntArray(5,100);
+       int[] b = randomIntArray(10,100);
        for(int i=0;i<b.length;i++){
          System.out.print(b[i]+",");
        }
-       System.out.println();
-       swap(b, 3, 4);
-       smallest(b, 1);
+       smallest(b, 0);
    }
 
    public static void pr(String str){
@@ -31,15 +29,12 @@ public class DD_Util_Ross_Two {
    public static void swap(int[] a, int i, int j){
       int arrLength = a.length;
       if ((arrLength - 1) < i || (arrLength - 1) < j) {
-        pr("One or more indices out of range.");
+        System.out.print("One or more indices out of range.");
       } else {
         int valOne = a[i];
         int valTwo = a[j];
         a[i] = valTwo;
         a[j] = valOne;
-        prl("Swapped the following values (at indices " + i + " and " + j + "):");
-        prl(valOne + " => " + valTwo);
-        prl(valTwo + " => " + valOne);
       }
    }
    
@@ -53,9 +48,8 @@ public class DD_Util_Ross_Two {
    */
    public static int[] randomIntArray(int length, int n){
       Random generator = new Random();
-      //int num = min + (int)Math.random();
       int[] a = new int[length];
-      for(int i=0; i < a.length; i++){
+      for(int i = 0; i < a.length; i++){
          a[i] = generator.nextInt(n);
       }  
       return a;
@@ -65,15 +59,16 @@ public class DD_Util_Ross_Two {
    an array
    @param arr, the int array to iterate through
    @param startIndex, an int value representing the index
-   at which to begin the tail range of the array
+   at which to begin the tail range of arr
    @return smallestIndex, an int value representing the index
    of the smallest element in the range from arr[startIndex] to
    arr[arr.length - 1]
    */
    public static int smallest(int[] arr, int startIndex) {
+      System.out.println();
       int arrLength = arr.length;
       if (startIndex > arrLength - 1) {
-        prl("Index out of range.");
+        return -1;
       } else {
         int smallest = arr[startIndex];
         int smallestIndex = startIndex;
@@ -84,7 +79,6 @@ public class DD_Util_Ross_Two {
             smallest = arr[i];
           }
         }
-        prl("Smallest value is " + smallest + " at index " + smallestIndex + " (in a range from " + startIndex + " to " + (arrLength - 1) + ").");
         return smallestIndex;
       }
       return -1;
